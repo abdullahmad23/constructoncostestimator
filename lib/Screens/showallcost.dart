22f62191d6f54796f5e77908caf4686f183b7d57
abdullah <abdullahmad246@gmail.com>
2024-01-19 08:06:21 +0500

    pdf working generated

--- lib/Screens/showallcost.dart
+++ lib/Screens/showallcost.dart
@@ -13,47 +13,17 @@ import 'package:flutter/material.dart';
 
 class ShowAllData extends StatefulWidget {
   Map<String, dynamic> rawData;
-  ShowAllData({super.key, required this.rawData});
+
+  ShowAllData({
+    super.key,
+    required this.rawData,
+  });
 
   @override
   State<ShowAllData> createState() => _ShowAllDataState();
 }
 
 class _ShowAllDataState extends State<ShowAllData> {
-  Map<String, dynamic> data = {
-    "Bricks": {"Quantity": result['NoOfBricks'], "Cost": result['costOfBrick']},
-    "Cement": {
-      "Quantity": result['NoOfCement'],
-      "Cost": result['CostOfCement']
-    },
-    "Sand": {"Quantity": result['NoOfSand'], "Cost": result['CostOfSand']},
-    "Crush": {"Quantity": 100, "Cost": 100},
-    "Labour": {"Quantity": 100, "Cost": 100},
-    "Steel": {"Quantity": 100, "Cost": 100},
-    "Material&Labour": {"Quantity": 100, "Cost": 100},
-    "Plumbing": {"Quantity": 100, "Cost": 100},
-    "Electrical": {"Quantity": 100, "Cost": 100},
-    "Glass": {"Quantity": 100, "Cost": 100},
-    "UtilitiesTotal": {"Quantity": 100, "Cost": 100},
-    "Windows": {"Quantity": 100, "Cost": 100},
-    "Dorrs": {"Quantity": 100, "Cost": 100},
-    "W&DTotal": {"Quantity": 100, "Cost": 100},
-    "Colouring": {"Quantity": 100, "Cost": 100},
-    "RoofDecoration": {"Quantity": 100, "Cost": 100},
-    "Washroom": {"Quantity": 100, "Cost": 100},
-    "Kitchens": {"Quantity": 100, "Cost": 100},
-    "Others": {"Quantity": 100, "Cost": 100},
-    "Total": {"Quantity": 100, "Cost": 100},
-    "TotalCost": {"Quantity": 00, "Cost": 100},
-
-
-  };
-  pdfPrint() {
-    generatePdf(data).then((_) {
-      print('PDF generated file.');
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -484,16 +454,6 @@ class _ShowAllDataState extends State<ShowAllData> {
                 ),
               ],
             ),
-            const SizedBox(
-              height: 15,
-            ),
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(MediaQuery.of(context).size.width * 0.5,
-                      MediaQuery.of(context).size.width * 0.13),
-                ),
-                onPressed: pdfPrint,
-                child: const Text('print')),
           ],
         ),
       )),
--- lib/testing.dart
+++ lib/testing.dart
@@ -1,3 +1,4 @@
+import 'package:constructoncostestimator/Screens/Print.dart';
 import 'package:constructoncostestimator/Screens/TotalBudget.dart';
 import 'package:flutter/material.dart';
 
@@ -18,8 +19,47 @@ class _TestingLabState extends State<TestingLab> {
     print(result['costOfBrick']);
   }
 
+  Map<String, dynamic> data = {
+    "Bricks": {"Quantity": result['NoOfBricks'], "Cost": result['costOfBrick']},
+    "Cement": {
+      "Quantity": result['NoOfCement'],
+      "Cost": result['CostOfCement']
+    },
+    "Sand": {"Quantity": result['NoOfSand'], "Cost": result['CostOfSand']},
+    "Crush": {"Quantity": 100, "Cost": 100},
+    "Labour": {"Quantity": 100, "Cost": 100},
+    "Steel": {"Quantity": 100, "Cost": 100},
+    "Material&Labour": {"Quantity": 100, "Cost": 100},
+    "Plumbing": {"Quantity": 100, "Cost": 100},
+    "Electrical": {"Quantity": 100, "Cost": 100},
+    "Glass": {"Quantity": 100, "Cost": 100},
+    "UtilitiesTotal": {"Quantity": 100, "Cost": 100},
+    "Windows": {"Quantity": 100, "Cost": 100},
+    "Dorrs": {"Quantity": 100, "Cost": 100},
+    "W&DTotal": {"Quantity": 100, "Cost": 100},
+    "Colouring": {"Quantity": 100, "Cost": 100},
+    "RoofDecoration": {"Quantity": 100, "Cost": 100},
+    "Washroom": {"Quantity": 100, "Cost": 100},
+    "Kitchens": {"Quantity": 100, "Cost": 100},
+    "Others": {"Quantity": 100, "Cost": 100},
+    "Total": {"Quantity": 100, "Cost": 100},
+    "TotalCost": {"Quantity": 00, "Cost": 100},
+  };
+  void pdfPrint() {
+    generatePdf(data).then((_) {
+      print('PDF generated file.');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: Text("btn"),
+          onPressed: pdfPrint,
+        ),
+      ),
+    );
   }
 }
